27332ec1206eb8dd00e692164327350c963b15a9
Vũ Phúc Trường An <kingdragon7a8@gmail.com>
2020-12-03 01:23:16 +0700

    one page scroll js added

--- js/index.js
+++ js/index.js
@@ -1,28 +1,15 @@
 window.onload = init;
 function init() {
-  let a = document.querySelector(".body-wrapper");
-  let b = document.querySelector(".demo");
-  //   document.body.onscroll = scroll;
-  function scroll() {
-    window.scrollTo(0, 900);
-  }
-  if (window.scrollY <= 900) {
-  }
-  let OldScrollHeight = 0;
-  document.body.onscroll = function () {
-    let scrollHeight = window.scrollY;
-    let contentShow = document.getElementById("content-wrapper");
-    contentShow.scrollIntoView({ behavior: "smooth" });
-    if (scrollHeight.onscroll <= 900) {
+  onePageScroll();
+}
+function onePageScroll() {
+  window.addEventListener("wheel", (e) => {
+    if (e.pageY <= 1120 && e.deltaY > 99) {
+      window.scroll(0, 900);
     }
-  };
-  //   a.onclick = function () {
-  //     if (window.scrollY <= 901) {
-  //       window.onscroll = window.scrollTo(0, 900);
-  //     }
-  //     let x = window.scrollY;
-  //     b.innerHTML = x;
-  //     console.log(a.scrollHeight);
-  //   };
-  //   document.body.scrollTop = 100;
+    if (e.pageY <= 1250 && e.deltaY < -99) {
+      console.log(e.pageY);
+      window.scroll(0, 0);
+    }
+  });
 }
