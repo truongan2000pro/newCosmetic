6b14371ba8bd3b5b0523baa980853697246a5f60
Vũ Phúc Trường An <kingdragon7a8@gmail.com>
2020-12-03 21:48:47 +0700

    added media bar with css

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function onePageScroll() {
     if (e.pageY <= 1120 && e.deltaY > 99) {
       window.scrollTo({ top: content.offsetTop - 100 });
     }
-    if (e.pageY <= content.offsetTop && e.deltaY < -99) {
+    if (e.pageY <= content.offsetTop + 200 && e.deltaY < -99) {
       console.log(e.pageY);
       window.scrollTo({ top: 0 });
     }
