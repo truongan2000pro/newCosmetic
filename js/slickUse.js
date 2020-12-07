d8463f7d307d92ad1fbd0c0cdcadade78bbc2e50
Vũ Phúc Trường An <kingdragon7a8@gmail.com>
2020-12-07 23:46:57 +0700

    slick js added

---
+++ js/slickUse.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".ol-content").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    // prevArrow: true,
+    // nextArrow: true,
+    // arrows: true,
+  });
+});
