8f74c9f0ef088bc9f3ebe9796cd4889461831506
Vũ Phúc Trường An <kingdragon7a8@gmail.com>
2020-12-18 01:33:12 +0700

    sign in sign up modal added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 window.onload = init;
 function init() {
   onePageScroll();
+  SignInSignUpSwitch();
 }
 function onePageScroll() {
   let content = document.getElementById("content-wrapper");
@@ -13,5 +14,21 @@ function onePageScroll() {
       console.log(e.pageY);
       window.scrollTo({ top: 0 });
     }
+  };
+}
+function SignInSignUpSwitch() {
+  let signUpLink = document.getElementsByClassName("link-sign-up")[0];
+  let signInLink = document.getElementsByClassName("link-log-in")[0];
+  let formLogIn = document.getElementsByClassName("log-in-wrapper")[0];
+  let formSignUp = document.getElementsByClassName("sign-up-wrapper")[0];
+  signUpLink.onclick = function () {
+    formLogIn.style.display = "none";
+    formSignUp.style.display = "block";
+    formSignUp.style.animation = "fade-in 1s";
+  };
+  signInLink.onclick = function () {
+    formLogIn.style.display = "block";
+    formSignUp.style.display = "none";
+    formLogIn.style.animation = "fade-in 1s";
   };
 }
